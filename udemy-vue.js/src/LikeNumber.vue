55f9a803b540f1382be696ffa54aba281cef6ee3
Taka2401 <tkht2401@gmail.com>
2021-08-21 13:01:57 +0900

    .Vue ローカル登録

---
+++ udemy-vue.js/src/LikeNumber.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>いいね({{ number }})</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 10
+    }
+  }
+}
+</script>
+
+<style>
+</style>
