4cfa6011f12caf3999d35782b05a213cbed66198
Taka2401 <tkht2401@gmail.com>
2021-08-24 21:00:17 +0900

    ESlint コード解析

--- udemy-vue.js/src/components/LikeNumber.vue
+++ udemy-vue.js/src/components/LikeNumber.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <p>いいね({{ halfNumber }})</p>
     <button @click="increment">+1</button>
   </div>
@@ -7,19 +7,18 @@
 
 <script>
 export default {
-  props: ['totalNumber'],
+  props: ["totalNumber"],
   computed: {
     halfNumber() {
       return this.totalNumber / 2;
     }
   },
   methods: {
-    increment: function() {
-      this.$emit('my-click', this.totalNumber += 1);
+    increment() {
+      this.$emit("my-click", this.totalNumber + 1);
     }
-  },
-}
+  }
+};
 </script>
 
-<style>
-</style>
+
