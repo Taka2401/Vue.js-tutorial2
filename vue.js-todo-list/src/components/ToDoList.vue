4d6d2f28727d94fc4480bad6b15f739e694038d3
Taka2401 <tkht2401@gmail.com>
2021-09-07 18:26:55 +0900

    ToDoアプリ完成

--- vue.js-todo-list/src/components/ToDoList.vue
+++ vue.js-todo-list/src/components/ToDoList.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="container">
     <h2>ToDoList</h2>
-    <input type="text">
-    <button @click="addItem">送信</button>
+    <form @submit.prevent>
+      <input type="text" v-model="newItem">
+      <button @click="addItem">送信</button>
+    </form>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo">{{ todo.item }}</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      newItem: '',
+      todos: []
+    }
+  },
+  methods: {
+    addItem() {
+      // alert('alert');
+      if(this.newItem === '') return;
+      const todo = {
+        item: this.newItem
+      };
+      this.todos.push(todo);
+      this.newItem = ''
+    }
+  }
+};
 </script>
 
 <style>
   .container {
-    text-align: center;
+    text-align: left;
   }
 </style>
