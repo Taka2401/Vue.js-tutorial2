4adf5962d7c768dd3d306b6c996c84ae214b60f1
Taka2401 <tkht2401@gmail.com>
2021-09-07 17:35:54 +0900

    投稿フォーム作成

---
+++ vue.js-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h2>ToDoList</h2>
+    <input type="text">
+    <button @click="addItem">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  .container {
+    text-align: center;
+  }
+</style>
--- vue.js-todo-list/src/main.js
+++ vue.js-todo-list/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ToDoList from './components/ToDoList.vue'
 
 Vue.config.productionTip = false
+Vue.component('ToDoList', ToDoList);
 
 new Vue({
   render: h => h(App),
